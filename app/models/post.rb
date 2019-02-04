2c4f74af7ce5d2fa69a1438c4f40072970fbc631
NorbertLam <noobertlm@gmail.com>
2019-02-04 15:30:09 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
+class BaitValidator < ActiveModel::Validator
+  def validate(record)
+    unless record.title != nil and record.title.starts_with? "You Won't Believe"
+      record.errors[:title] << "Needs bait"
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates_with BaitValidator
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
 end
