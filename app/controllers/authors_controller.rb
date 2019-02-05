4561a01a6ac25f60d92c117e44757ce9c982a40f
Elizabeth Le <elizabethlemk@yahoo.com>
2019-02-05 10:17:00 -0500

    Done.

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,22 @@
+class AuthorsController < ApplicationController
+
+
+  def show
+    @author = Author.find(params[:id])
+  end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.create(author_params)
+    redirect_to author_path(@author)
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:name, :phone_number)
+  end
+end
