590946d9eae36eb296d27a9ea59345cfb88043a9
edwinlin <edwinlin@gmail.com>
2019-02-05 01:30:16 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true
+	validates :content, length: {minimum: 10}
+    validates :summary, length: { maximum: 20 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not listed" }
+  
+  validate :is_clickbait?
+
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
+
+  def is_clickbait?
+  	clickbait = [/Won't Believe/, /Secret/, /Top [number]/, /Guess/]
+
+    if clickbait.none? { |pattern| pattern.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
